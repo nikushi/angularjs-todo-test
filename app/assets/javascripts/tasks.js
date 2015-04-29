1b57ecfa78d28df09989bd9e18a1a6a1600948cd
Nobuhiro Nikushi <deneb.ge@gmail.com>
2015-04-29 21:50:17 +0900

    Load tasks from the backend on loading index

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,13 +1,16 @@
 angular.module('todoApp', [])
-  .controller('TasksController', function() {
+  .controller('TasksController', ['$scope', '$http', function($scope, $http) {
     var tasksCtrl = this;
-    tasksCtrl.tasks = [
-      {title:'learn angular', done:true},
-      {title:'build an angular app', done:false}];
- 
+
+    tasksCtrl.indexurl = '/tasks.json';
+
+    $http({method: 'GET', url: tasksCtrl.indexurl}).success(function(data, status) {
+      tasksCtrl.tasks = data;
+    });
+
     tasksCtrl.addTask = function() {
       tasksCtrl.tasks.push({title:tasksCtrl.taskTitle, done:false});
       tasksCtrl.taskTitle = '';
     };
- 
-  });
+
+  }]);
