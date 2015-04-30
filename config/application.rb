653f2f9695a9a09b04a410e6760d420529ddeb41
Nobuhiro Nikushi <deneb.ge@gmail.com>
2015-04-30 10:58:48 +0900

    Did not need to add additional asset path

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module MyToDo
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    # config.assets.paths << 'path/to/assets'
   end
 end
